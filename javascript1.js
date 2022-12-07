044b3a918d3c1f52ccd0f2a9564275615b974ca1
anand2812 <115833662+anand2812@users.noreply.github.com>
2022-12-07 15:31:56 +0530

    Add files via upload

---
+++ javascript1.js
@@ -0,0 +1,17 @@
+var x = 5;
+var y = 10;
+var z = x+y
+document.write("the sum of x and y is:",z)
+document.write("</br>")
+var w = x*y
+document.write("the product of x and y is:",w)
+document.write("</br>")
+var r = x/y
+document.write("the divison of x and y is:",r)
+document.write("</br>")
+var r = x//y
+document.write("the modulas of x and y is:",r)
+document.write("</br>")
+
+
+
